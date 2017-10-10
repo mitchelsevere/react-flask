454aff23a4bcec9fa81f7ace9ad94de7a3ccdcfe
Mitchel Severe <truth531@gmail.com>
2017-10-10 13:12:47 -0400

    add react render

--- fullstack/static/src/index.js
+++ fullstack/static/src/index.js
@@ -1,1 +1,8 @@
-alert("Hello World!");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
